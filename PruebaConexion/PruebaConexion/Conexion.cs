47f9c7aa9af040866831491bf0e60dae657b06af
Matias La Ferraro <matias.lf@hotmail.com>
2014-04-27 18:31:57 -0300

    prueba de conexion
    solo para saber si anda el repo, hay que armar el SRC y todas las
    carpetas

---
+++ PruebaConexion/PruebaConexion/Conexion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    class Conexion
+    {
+        public static SqlConnection getConexion()
+        {
+
+            SqlConnection conn = new SqlConnection(@"Data Source= localhost\SQLSERVER2008; Initial Catalog = GD1C2014; User Id= sa; Password = gestiondedatos");
+            conn.Open();
+            return conn;
+
+
+        }
+    }
+}
---
+++ PruebaConexion/PruebaConexion/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (Conexion.getConexion() != null)
+                MessageBox.Show("conexion exitosa");
+            else
+                MessageBox.Show("conexion fallida");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
